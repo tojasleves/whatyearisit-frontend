bd318e38cb28bb12a37472fc57c7c903ea1cc1e4
erikaleon <erikaleon2@gmail.com>
2023-09-15 14:55:59 +0200

    changed fetched URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('https://github.com/tojasleves/whatyearisit-backend.git')
+fetch('https://whatyearisit-backend-ashen-two.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.year;
